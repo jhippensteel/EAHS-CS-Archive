51713e04c8e8a3a5448e30ac581f51f926da608b
Jaysen Hippensteel <105116606+jhippensteel@users.noreply.github.com>
2023-05-25 20:16:27 -0400

    ITPP m5a "gone bananas"
    Demonstrates div and mod operators.

---
+++ m5a_gone_bananas/main.py
@@ -0,0 +1,24 @@
+bunch = int(input("Enter an integer: "))
+print()
+print("A bunch of", bunch, "bananas could make...")
+co2 = bunch // 2
+i2 = bunch % 2
+print(co2, "clusters of 2 bananas with", i2, "banana(s) left over")
+co3 = bunch // 3
+i3 = bunch % 3
+print(co3, "clusters of 3 bananas with", i3, "banana(s) left over")
+x = bunch // 4
+y = bunch % 4
+print(x, "clusters of 4 bananas with", y, "banana(s) left over")
+x = bunch // 5
+y = bunch % 5
+print(x, "clusters of 5 bananas with", y, "banana(s) left over")
+x = bunch // 6
+y = bunch % 6
+print(x, "clusters of 6 bananas with", y, "banana(s) left over")
+x = bunch // 7
+y = bunch % 7
+print(x, "clusters of 7 bananas with", y, "banana(s) left over")
+x = bunch // 8
+y = bunch % 8
+print(x, "clusters of 8 bananas with", y, "banana(s) left over")
